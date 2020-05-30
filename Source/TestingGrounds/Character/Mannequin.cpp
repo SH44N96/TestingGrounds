ad4872f21f29b7a19e6868d912f0503de471cf0d
SH44N96 <sh44n96@users.noreply.github.com>
2020-05-30 12:12:12 +0800

    Converting a Character BP to C++

---
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mannequin.h"
+#include "../Weapons/Gun.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a CameraComponent
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.55f, 1.75f, 65.f)); // Position the Camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetRelativeRotation(FRotator(2.f, -19.25f, 5.25f));
+	Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.5f, -155.75f));
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	if(GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun Blueprint Missing"));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); // Attach Gun Mesh Component
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMannequin::Fire()
+{
+	Gun->OnFire();
+}
